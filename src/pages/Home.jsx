b272448a1712b92be6cc8a6d781e7e0cc9c9775f
Kedehboy <ayangcostly@gmail.com>
2022-12-23 01:45:00 +0100

    Pushing first push

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,40 +1,36 @@
-import React from 'react';
+import React from 'react'
 
-import Header from '../partials/Header';
-import HeroHome from '../partials/HeroHome';
-import FeaturesHome from '../partials/Features';
-import FeaturesBlocks from '../partials/FeaturesBlocks';
-import Testimonials from '../partials/Testimonials';
-import Newsletter from '../partials/Newsletter';
-import Footer from '../partials/Footer';
-import Banner from '../partials/Banner';
+import Header from '../partials/Header'
+import HeroHome from '../partials/HeroHome'
+import FeaturesHome from '../partials/Features'
+import FeaturesBlocks from '../partials/FeaturesBlocks'
+import Testimonials from '../partials/Testimonials'
+import Newsletter from '../partials/Newsletter'
+import Footer from '../partials/Footer'
+import Banner from '../partials/Banner'
 
 function Home() {
   return (
-    <div className="flex flex-col min-h-screen overflow-hidden">
-
+    <div className='flex flex-col min-h-screen overflow-hidden'>
       {/*  Site header */}
       <Header />
 
       {/*  Page content */}
-      <main className="flex-grow">
-
+      <main className='flex-grow'>
         {/*  Page sections */}
         <HeroHome />
         <FeaturesHome />
-        <FeaturesBlocks />
+        {/* <FeaturesBlocks /> */}
         <Testimonials />
-        <Newsletter />
-
+        {/* <Newsletter /> */}
       </main>
 
-      <Banner />
+      {/* <Banner /> */}
 
       {/*  Site footer */}
       <Footer />
-
     </div>
-  );
+  )
 }
 
-export default Home;
+export default Home
